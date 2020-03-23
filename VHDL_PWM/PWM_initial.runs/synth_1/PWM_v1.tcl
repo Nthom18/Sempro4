d3e65d1fcb693d29fd44dd705dc1b9c4bc3f27e2
Charlotte <chnur18@student.sdu.dk>
2020-03-23 10:46:04 +0100

    PWM on FPGA
    PWM on FPGA rettet ved at tilføje Basys3filen til projektet

--- VHDL_PWM/PWM_initial.runs/synth_1/PWM_v1.tcl
+++ VHDL_PWM/PWM_initial.runs/synth_1/PWM_v1.tcl
@@ -17,20 +17,18 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param chipscope.maxJobs 2
-set_param xicom.use_bs_reader 1
 create_project -in_memory -part xc7a35tcpg236-1
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir C:/Users/dotte/Documents/Projekter_DIG/PWM_initial/PWM_initial.cache/wt [current_project]
-set_property parent.project_path C:/Users/dotte/Documents/Projekter_DIG/PWM_initial/PWM_initial.xpr [current_project]
+set_property webtalk.parent_dir C:/Users/dotte/Documents/Sempro4/VHDL_PWM/PWM_initial.cache/wt [current_project]
+set_property parent.project_path C:/Users/dotte/Documents/Sempro4/VHDL_PWM/PWM_initial.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language VHDL [current_project]
-set_property ip_output_repo c:/Users/dotte/Documents/Projekter_DIG/PWM_initial/PWM_initial.cache/ip [current_project]
+set_property ip_output_repo c:/Users/dotte/Documents/Sempro4/VHDL_PWM/PWM_initial.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_vhdl -library xil_defaultlib C:/Users/dotte/Documents/Projekter_DIG/PWM_initial/PWM_initial.srcs/sources_1/new/PWM.vhd
+read_vhdl -library xil_defaultlib C:/Users/dotte/Documents/Sempro4/VHDL_PWM/PWM_initial.srcs/sources_1/new/PWM.vhd
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
 # design are intentionally left as such for best results. Dcp files will be
@@ -39,8 +37,8 @@ read_vhdl -library xil_defaultlib C:/Users/dotte/Documents/Projekter_DIG/PWM_ini
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-read_xdc C:/Users/dotte/Documents/Projekter_DIG/Komigang_2/Includes/Basys3_Master.xdc
-set_property used_in_implementation false [get_files C:/Users/dotte/Documents/Projekter_DIG/Komigang_2/Includes/Basys3_Master.xdc]
+read_xdc C:/Users/dotte/Documents/Sempro4/VHDL_PWM/PWM_initial.srcs/constr_1/imports/Basys3_Master.xdc
+set_property used_in_implementation false [get_files C:/Users/dotte/Documents/Sempro4/VHDL_PWM/PWM_initial.srcs/constr_1/imports/Basys3_Master.xdc]
 
 set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
